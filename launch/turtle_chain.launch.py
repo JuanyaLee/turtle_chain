a2a947624b368f40678e5e5a25c49e918857f714
JuanyaLee <164323498+JuanyaLee@users.noreply.github.com>
2026-05-10 10:12:01 +0800

    Update turtle_chain.launch.py

--- launch/turtle_chain.launch.py
+++ launch/turtle_chain.launch.py
@@ -2,40 +2,15 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 
-
 def generate_launch_description():
-
 
     return LaunchDescription([
 
         Node(
             package='turtle_chain',
-            executable='follower',
-            name='follower_2',
-            parameters=[{
-                'follower': 'turtle2',
-                'target': 'turtle1'
-            }]
-        ),
-
-        Node(
-            package='turtle_chain',
-            executable='follower',
-            name='follower_3',
-            parameters=[{
-                'follower': 'turtle3',
-                'target': 'turtle2'
-            }]
-        ),
-
-        Node(
-            package='turtle_chain',
-            executable='follower',
-            name='follower_4',
-            parameters=[{
-                'follower': 'turtle4',
-                'target': 'turtle3'
-            }]
+            executable='chain_manager',
+            name='chain_manager',
+            output='screen'
         )
 
     ])
